f59364223092e4ced22e9e25fd7e8c794b53b42f
Talibiq123 <talibsaeed90@gmail.com>
2025-03-16 12:58:04 +0530

    user model

--- backend/models/todos/user.models.js
+++ backend/models/todos/user.models.js
@@ -1,7 +1,23 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    
-})
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email address"]
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
 
-export const user = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
